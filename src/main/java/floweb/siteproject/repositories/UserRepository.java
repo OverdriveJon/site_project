827fb37a9e7c773483e00adf4768031b8782b108
Joao Victor <jvoliveiraresende@hotmail.com>
2020-10-19 13:42:06 -0300

    Service e metodos Get

--- src/main/java/floweb/siteproject/repositories/UserRepository.java
+++ src/main/java/floweb/siteproject/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package floweb.siteproject.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import floweb.siteproject.entities.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
--- src/main/java/floweb/siteproject/resources/UserResource.java
+++ src/main/java/floweb/siteproject/resources/UserResource.java
@@ -1,23 +1,36 @@
 package floweb.siteproject.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import floweb.siteproject.entities.User;
+import floweb.siteproject.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	
+	@Autowired
+	private UserService service;
 
 	public UserResource() {
 		// TODO Auto-generated constructor stub
 	}
 	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "999334", "mariazinah");
-		return ResponseEntity.ok().body(u);}
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);}
 
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
 }
